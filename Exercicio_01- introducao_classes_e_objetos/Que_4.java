fdd50fb143b6e061a1c06092ef12203baac2818f
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2025-08-21 14:55:29 -0300

    Q4_respondida

--- Exercicio_01- introducao_classes_e_objetos/Que_4.java
+++ Exercicio_01- introducao_classes_e_objetos/Que_4.java
@@ -1,3 +1,34 @@
-public class Que_4 {
+// Exercício: pegue trechos de código da “piscina” e coloque-os nas
+// linhas em branco do código para produzir o resultado abaixo.
+class PoolPuzzleOne {
+    public static void main(String [] args) {
+        int x = 0;
 
+        while (x < 4) { //espaço 1
+
+            System.out.print("a");
+
+            if (x < 1) {
+                System.out.print(" "); //espaço 2
+            }
+
+            System.out.print("n"); // espaço 3
+
+            if (x > 1) { //espaço 4
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+
+            if (x == 1) {
+                System.out.print("noys"); //espaço 5
+            }
+
+            if (x < 1) { //espaço 6
+                System.out.print("oise");
+            }
+
+            System.out.println(); //espaço 6
+            x = x + 1;
+        }
+    }
 }
