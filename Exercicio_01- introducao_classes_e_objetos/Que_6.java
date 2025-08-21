b0c459169c6b1ad4f4911160549997e083f6889f
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2025-08-21 15:06:20 -0300

    Q6_Resposta

--- Exercicio_01- introducao_classes_e_objetos/Que_6.java
+++ Exercicio_01- introducao_classes_e_objetos/Que_6.java
@@ -1,1 +1,29 @@
 // Aluno: Marcelo Augusto de Barros Araújo.
+// Exercício: reorganize os trechos de código para criar um programa Java
+// funcional que produza o resultado abaixo.
+
+class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+}
+
+class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+
+        if (d.snare == true) {
+            d.playSnare();
+        }
+
+        d.snare = false;
+        d.playTopHat();
+    }
+}
