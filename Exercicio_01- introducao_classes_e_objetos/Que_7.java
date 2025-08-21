fc5f1c9e8402a61fb4db79a2f29b3471dfa12d13
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2025-08-21 15:12:19 -0300

    Questão7-respondida

--- Exercicio_01- introducao_classes_e_objetos/Que_7.java
+++ Exercicio_01- introducao_classes_e_objetos/Que_7.java
@@ -1,1 +1,35 @@
 // Aluno: Marcelo Augusto de Barros Araújo.
+// Exercício: pegue trechos de código da “piscina” e coloque-os nas linhas
+// em branco do código para produzir o resultado abaixo.
+
+
+public class EchoTestDrive {
+    public static void main (String [] args) {
+        Echo e1 = new Echo();
+        Echo e2 = new Echo(); // Alterado para new Echo() para que a lógica funcione
+        int x = 0;
+        while ( x < 4 ) {
+            e1.hello();
+            e1.count = e1.count + 1;
+
+            if ( x == 3 ) { // Na imagem, a condição em branco seria x == 3
+                e2.count = e2.count + 1;
+            }
+            if ( x > 0 ) { // Na imagem, a condição em branco seria x > 0
+                e2.count = e2.count + e1.count;
+            }
+            x = x + 1;
+        }
+        System.out.println(e2.count);
+    }
+}
+//--------------------------------------------------------------------------------
+class Echo {
+    int count = 0;
+    void hello() {
+        System.out.println("helloooo... ");
+    }
+}
+
+//OBS: separar ambas as classes como EchoTestDrive.java e Echo.java para conseguir executar
+// e ter interação entre elas.
