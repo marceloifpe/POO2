7fc50f20b708ac4cace9decd3a72744e17a6bde1
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2025-09-25 19:15:38 -0300

    Q2_feito

--- Exercicio_04-escrevendo_um_programa/Q2/MultiFor/src/App.java
+++ Exercicio_04-escrevendo_um_programa/Q2/MultiFor/src/App.java
@@ -1,5 +1,15 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+// Exercício: reconstrua os trechos de código para criar um
+// programa Java funcional que produza o resultado listado abaixo:
+
+class MultiFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > 2; j--) {
+                System.out.println(i + " " + j);
+            }
+            if (i == 1) {
+                i++;
+            }
+        }
     }
 }
