1ad5f26aa9282d444e4cbda37e414d64143f1a14
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2025-11-19 12:16:28 -0300

    Q5_funcional

---
+++ Exercicio_09-tratamento_de_erros/Q5/Juntando_Teste/src/ExTestDrive.java
@@ -0,0 +1,31 @@
+class MyEx extends Exception { }
+
+public class ExTestDrive {
+
+    public static void main(String[] args) {
+        String test = args[0];
+
+        try {
+            System.out.print("t");
+            doRisky(test);
+            System.out.print("o");
+
+        } catch (MyEx e) {
+        } finally {
+            System.out.print("w");
+        }
+
+        System.out.println("s");
+    }
+
+    static void doRisky(String t) throws MyEx {
+        System.out.print("h");
+
+        if ("yes".equals(t)) {
+            System.out.print("a");
+            throw new MyEx();
+        }
+
+        System.out.print("r");
+    }
+}
